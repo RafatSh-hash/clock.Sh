208f16882e02495b0a7525dd6d4cd010e973c283
RafatSh-Hash <rafatsh800@gmail.com>
2023-08-20 18:14:25 +0600

    ---

--- script.js
+++ script.js
@@ -5,14 +5,26 @@ const nextBtn = document.getElementById("nextBtn");
 let currentIndex = 0;
 
 prevBtn.addEventListener("click", () => {
-  carouselItems[currentIndex].classList.remove("active");
-  currentIndex =
-    (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-  carouselItems[currentIndex].classList.add("active");
+  changeSlide(currentIndex - 1);
 });
 
 nextBtn.addEventListener("click", () => {
-  carouselItems[currentIndex].classList.remove("active");
-  currentIndex = (currentIndex + 1) % carouselItems.length;
-  carouselItems[currentIndex].classList.add("active");
+  changeSlide(currentIndex + 1);
 });
+
+carousel.addEventListener("wheel", (event) => {
+  if (event.deltaY < 0) {
+    changeSlide(currentIndex - 1);
+  } else {
+    changeSlide(currentIndex + 1);
+  }
+});
+
+function changeSlide(newIndex) {
+  carouselItems[currentIndex].classList.remove("active");
+  currentIndex = (newIndex + carouselItems.length) % carouselItems.length;
+  carouselItems[currentIndex].classList.add("active");
+}
+
+// Set the initial active slide
+carouselItems[currentIndex].classList.add("active");
